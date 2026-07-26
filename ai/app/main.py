34518e481e2d3e0d2d1423b2cb051de109b34cb7
Ubuntu <ubuntu@ip-172-31-25-99.ap-southeast-2.compute.internal>
2026-07-26 01:44:17 +0000

    画像アップロードAPI実装

--- ai/app/main.py
+++ ai/app/main.py
@@ -19,7 +19,11 @@ ALLOWED_CONTENT_TYPES ={
     "image/WEBP":".WEBP"
 }
 
-
+@app.get("/")
+def root():
+    return{
+        "message":"Counter Stock AI API in running"
+    }
 
 @app.get("/health")
 def health_check():
@@ -28,4 +32,51 @@ def health_check():
         "service": "counter-stock-ai",
     }
 
+@app.post("/upload")
+async def upload_image(
+    file:UploadFile = File(...)
+):
+
+    if file.content_type not in ALLOWED_CONTENT_TYPES:
+        raise HTTPException(
+            status_code = 400,
+            detail = "JPEG、PNG、WebP形式の画像を送信してください",
+        )
+
+    extension = ALLOWED_CONTENT_TYPES[file.content_type]
+    saved_filename = f"{uuid4().hex}{extension}"
+    saved_path = UPLOAD_DIR/saved_filename
+
+    try:
+        file_data = await file.read()
+
+        if not file_data:
+            raise HTTPException(
+                status_code = 400,
+                detail = "ファイル内容が空です"
+            )
+
+        with saved_patth.open("wb") as output_file:
+            output_file.write(file_data)
+
+
+    except HTTPException:
+        raise
+
+    except Exception as error:
+        raise HTTPException(
+            status_code = 500,
+            detail = f"画像の保存に失敗しました:{error}",
+        ) from error
+
+    finally:
+        await file.close()
+
+    return {
+        "message":"画像を受信しました",
+        "original_filename":"file.filename",
+        "saved_filename":"saved_filename",
+        "countent_type":"file.countent_type",
+        "size":"len(file_data)"
+    }
 
